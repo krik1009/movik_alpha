3da423c931e66c26c630e1daa7882d18d987a746
Kuriko IWAI <kuriko10091009@gmail.com>
2020-07-11 13:35:48 +0900

    build;

---
+++ frontend/build/precache-manifest.2fcf27525c7dd4f0e99e7e350b1cda50.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c923cfd1f42ddb040e6d8be13426451",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad64421f9e95c7219315",
+    "url": "/static/css/2.1a0a2c47.chunk.css"
+  },
+  {
+    "revision": "f2dd369907ef3ac878de",
+    "url": "/static/css/main.740edb06.chunk.css"
+  },
+  {
+    "revision": "ad64421f9e95c7219315",
+    "url": "/static/js/2.1cce1f0e.chunk.js"
+  },
+  {
+    "revision": "4816ecf85d678549dadb3aea1c680bf0",
+    "url": "/static/js/2.1cce1f0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2dd369907ef3ac878de",
+    "url": "/static/js/main.b9f9edef.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  },
+  {
+    "revision": "e89647fd19531cb11dcce64b5d2e5174",
+    "url": "/static/media/about1_creators.e89647fd.png"
+  },
+  {
+    "revision": "8d424c946b75f47a005b2092a3ce1aab",
+    "url": "/static/media/about1_viewers.8d424c94.png"
+  }
+]);
