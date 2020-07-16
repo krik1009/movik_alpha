f359e6a40ca2411af921d6a7782d2e6634aadc9d
Kuriko IWAI <kuriko10091009@gmail.com>
2020-07-16 21:45:48 +0900

    setting

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from dotenv import Dotenv
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
@@ -14,6 +14,11 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    
+
+    dotenv = Dotenv(os.path.join(os.path.dirname(__file__), '.env'))
+    os.environ.update(dotenv)
+    
     execute_from_command_line(sys.argv)
 
 
